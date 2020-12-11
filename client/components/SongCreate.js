6b75050c0688acfb6bd0beb45d593892a8a76b53
KelvinMitaki <mitakikelvin1@gmail.com>
2020-12-11 22:56:11 +0300

    added songcreate mutation

--- client/components/SongCreate.js
+++ client/components/SongCreate.js
@@ -1,20 +1,47 @@
-import React, { useState } from "react";
+import gql from "graphql-tag";
+import React from "react";
+import { graphql } from "react-apollo";
 
-const SongCreate = () => {
-  const [input, setInput] = useState("");
-  return (
-    <div className="container">
-      <h3>Create new song</h3>
-      <form>
-        <label>Song Title: </label>
-        <input
-          type="text"
-          onChange={e => setInput(e.target.value)}
-          value={input}
-        />
-      </form>
-    </div>
-  );
-};
+class SongCreate extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: ""
+    };
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+  onSubmit() {
+    e.preventDefault();
+    props.mutate({
+      variables: {
+        title: this.state.title
+      }
+    });
+  }
+  render() {
+    return (
+      <div className="container">
+        <h3>Create new song</h3>
+        <form onSubmit={this.onSubmit}>
+          <label>Song Title: </label>
+          <input
+            type="text"
+            onChange={e => this.setState(e.target.value)}
+            value={this.state.title}
+          />
+        </form>
+      </div>
+    );
+  }
+}
 
-export default SongCreate;
+const mutation = gql`
+  mutation AddSong($title: String) {
+    addSong(title: $title) {
+      id
+      title
+    }
+  }
+`;
+
+export default graphql(mutation)(SongCreate);
