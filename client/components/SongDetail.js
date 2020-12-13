7e2829d3be890d6866d7740c238e7d1c99b5e3ab
KelvinMitaki <mitakikelvin1@gmail.com>
2020-12-13 16:13:14 +0300

    wired up liking a lyric

--- client/components/SongDetail.js
+++ client/components/SongDetail.js
@@ -3,23 +3,63 @@ import { graphql } from "react-apollo";
 import { fetchSongDetails } from "../queries/fetchSongDetails";
 import { Link } from "react-router";
 import LyricCreate from "./LyricCreate";
+import gql from "graphql-tag";
 
-const SongDetail = ({ data }) => {
+const SongDetail = ({ data, mutate }) => {
   if (!data.song) {
     return <div>Loading...</div>;
   }
+  const likeLyric = id => {
+    mutate({
+      variables: {
+        id
+      }
+    });
+  };
   return (
     <div className="container">
       <Link to="/">Back</Link>
       <h3>{data.song.title}</h3>
-      {data.song.lyrics.map(lrc => (
-        <p key={lrc.id}>{lrc.content}</p>
-      ))}
+      <ul className="collection">
+        {data.song.lyrics.map(lrc => (
+          <li
+            className="collection-item"
+            key={lrc.id}
+            style={{ position: "relative" }}
+          >
+            {lrc.content}
+            <i
+              className="material-icons"
+              style={{
+                position: "absolute",
+                right: "5px",
+                cursor: "pointer",
+                color: lrc.likes !== 0 ? "blue" : "black"
+              }}
+              onClick={() => likeLyric(lrc.id)}
+            >
+              thumb_up
+            </i>
+          </li>
+        ))}
+      </ul>
       <LyricCreate />
     </div>
   );
 };
 
-export default graphql(fetchSongDetails, {
-  options: props => ({ variables: { id: props.params.id } })
-})(SongDetail);
+const mutation = gql`
+  mutation LikeLyric($id: ID!) {
+    likeLyric(id: $id) {
+      id
+      content
+      likes
+    }
+  }
+`;
+
+export default graphql(mutation)(
+  graphql(fetchSongDetails, {
+    options: props => ({ variables: { id: props.params.id } })
+  })(SongDetail)
+);
--- client/queries/fetchSongDetails.js
+++ client/queries/fetchSongDetails.js
@@ -8,6 +8,7 @@ export const fetchSongDetails = gql`
       lyrics {
         id
         content
+        likes
       }
     }
   }
